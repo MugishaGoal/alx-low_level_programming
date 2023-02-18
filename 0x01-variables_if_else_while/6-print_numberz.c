fb6dec2269e0728679f8a8602001ee66aeadc445
MugishaGoal <degaullemugisha@gmail.com>
2023-02-18 14:53:48 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n < '9'; n++)
+	putchar(n);
+	putchar('\n');
+
+	return (0);
+
+}
+	
