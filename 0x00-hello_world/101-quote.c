39f5d0bd5d25794de69d8cc668b02e1b5057de52
MugishaGoal <degaullemugisha@gmail.com>
2023-02-20 14:02:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,10 @@
 /**
  * main - Entry point
  *
- * Result: Always 1 (Error)
- *
+ * Return: Always 1 (Error)
  */
-
 int main(void)
-
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	return (1);
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
 }
