65eecdc72f7405d279cf8d02b9c670c844744867
MugishaGoal <degaullemugisha@gmail.com>
2023-02-20 12:14:54 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints value with proper grammar, but the outcome is a piece of art,
+ * main - Entry point
  *
- * Retuns: Always 0.
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-	printf("This program prints exactly with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
