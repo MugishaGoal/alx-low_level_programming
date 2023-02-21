55b092bffe37a04138dabfbd889032b5479d130c
MugishaGoal <degaullemugisha@gmail.com>
2023-02-21 15:40:45 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,23 +3,17 @@
 /**
  * print_alphabet - prints the lowercase alphabet followed by a new line
  *
- * Description: Prints the alphabet
- *              in ascending order, followed by a newline character.
- *              Uses the _putchar function to print each character.
- *
+ * Description: Prints the 26 lowercase letters of the alphabet
+ *		in ascending order, followed by a newline character.
+ *		Uses the _putchar function to print each character.
  */
-
 void print_alphabet(void)
-
 {
-
 	char letter = 'a';
-
 	while (letter <= 'z')
 	{
-	_putchar(letter);
-        letter++;
+		_putchar(letter);
+		letter++;
 	}
 	_putchar('\n');
-
 }
