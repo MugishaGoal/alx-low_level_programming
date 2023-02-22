08b849108ccc5c162a870524ba38eccd606efaf3
MugishaGoal <degaullemugisha@gmail.com>
2023-02-22 06:31:14 -0800

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _lowercase - checks for lowercase character
+ *
+ * Return: 1 for lowercase character otherwise 0
+ *
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+	return 1;
+
+	else 
+	{
+	return 0;
+	}
+}
