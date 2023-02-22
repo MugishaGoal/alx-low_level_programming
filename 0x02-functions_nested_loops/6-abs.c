05b2dbd967c5aedab46e336e16fff0dbc1432f9a
MugishaGoal <degaullemugisha@gmail.com>
2023-02-22 10:59:25 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * integer - computes the absolute value of an integer
+ * @n: the number to be checked
+ * Return: negative or postive value
+ *
+ */
+
+int _abs(int n)
+
+{
+	if (n < '0')
+	{
+	return -n;
+	}
+	else
+	{
+	return n;
+	}
+
+}
