bd0597186648d85a016cb5880e1e94d6668516ae
MugishaGoal <degaullemugisha@gmail.com>
2023-02-23 09:18:53 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: 0 Success (Always)
+ *
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
