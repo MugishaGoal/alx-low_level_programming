27238676b602bd7af323cf4bc0b59ce16c3730b9
MugishaGoal <degaullemugisha@gmail.com>
2023-02-23 15:52:34 -0800

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main - prints FizzBuzz program
+ *
+ * Return: 0 Succcess (Always)
+ *
+ */
+
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	if (x % 3 == 0 && x % 5 == 0)
+	{
+	printf("FizzBuzzz ");
+	}
+	else if (x % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else if (x % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%d ", x);
+	}
+	printf("\n");
+
+	return (0);
+}
