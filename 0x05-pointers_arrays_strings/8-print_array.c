10cdfe6a17922b961f280fff080106140a63cd1f
MugishaGoal <degaullemugisha@gmail.com>
2023-02-28 08:40:32 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,6 +6,9 @@
  * print_array - prints n elements of an array of integers
  * @a: array of integers
  * @n: Number of elements of the array to be printed
+ *
+ * This function takes an array of integers and the number of elements in the array,
+ * and prints each element separated by a comma and a space
  * Return: void
  */
 
