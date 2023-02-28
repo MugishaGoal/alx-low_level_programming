3910b79c434eeed83a1888013b5c75e6d48b5ae8
MugishaGoal <degaullemugisha@gmail.com>
2023-02-28 08:44:13 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,9 +6,10 @@
  * print_array - prints n elements of an array of integers
  * @a: array of integers
  * @n: Number of elements of the array to be printed
+ * Description: This function prints the elements of the array @a, up to a
+ * maximum of @n elements. Each element is separated by a comma, followed by
+ * a space, and the entire array is enclosed in square brackets.
  *
- * This function takes an array of integers and the number of elements in the array,
- * and prints each element separated by a comma and a space
  * Return: void
  */
 
