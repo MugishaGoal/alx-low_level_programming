ab784101fb38456832d8f385fcee130407ffe33e
MugishaGoal <degaullemugisha@gmail.com>
2023-03-02 11:34:27 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,6 @@ char *leet(char *str)
 	char *s = str;
 	char leet_dict[256] = {0};
 	int i, j;
-
 	const char *letters = "aAeEoOtTlL";
 	const char *codes = "4433007711";
 
@@ -24,10 +23,8 @@ char *leet(char *str)
 	{
 	j = (int)*s;
 	if (leet_dict != 0)
-	{
 	*s = leet_dict[j];
 	}
 	s++;
-	}
 	return (str);
 }
