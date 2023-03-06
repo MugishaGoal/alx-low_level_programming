cc5a40b35f3058213162708a316f7849dd42e394
MugishaGoal <degaullemugisha@gmail.com>
2023-03-06 08:36:43 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,21 +14,18 @@
 char *_strstr(char *haystack, char *needle)
 
 {
-	int i, j;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-	for (j = 0; needle[j] != '\0'; j++)
-	{
-	if (haystack[i + j] != needle[j])
-	{
-	break;
-	}
-	if (needle[j] == '\0')
-	{
-	return (&haystack[i]);
-	}
-	}
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
 	}
 	return (NULL);
 }
