0c80898630894ae5499887de0245437e2ab85fcc
MugishaGoal <degaullemugisha@gmail.com>
2023-03-06 05:17:12 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @n: number of bytes of the memory
+ * @s: pointer
+ * @b: the constant byte
+ *
+ * Return: a pointer to the memory area s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+}
