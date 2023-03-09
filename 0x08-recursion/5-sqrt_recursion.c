2ac7a7808c42fb036958cca6f56964a677a0f24b
MugishaGoal <degaullemugisha@gmail.com>
2023-03-09 14:29:01 -0800

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,23 +12,13 @@ int _sqrt_recursion(int n)
 	int i;
 
 	if (n < 0)
-	{
 	return (-1);
-	}
-	else if (n == 0)
-	{
-	return (0);
-	}
-	else
-	{
-	while (i * i <= n)
-	{
+	if (n == 0 || n == 1)
+	return (n);
+	return (_sqrt_recursion(n, 1));
 	if (i * i == n)
-	{
 	return (i);
-	}
-	i++;
-	}
+	if (i * i > n)
 	return (-1);
-	}
+	return (find_sqrt(n, i + 1));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number -  returns if the input integer is a prime number
+ * @n: the number to be checked
+ * Return: integer value
+ */
+
+int check_prime(int n, int i);
+
+int is_prime_number(int n)
+
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - check if a number is prime
+ * @n: the number to be checked
+ * @i: the iteration times
+ *
+ * Return: 1 for prime, otherwise 0
+ */
+
+int check_prime(int n, int i)
+
+{
+	if (n <= 1)
+	return (0);
+	if (n % i == 0 && i > 1)
+	return (0);
+	if ((n / i) < i)
+	return (1);
+	return (check_prime(n, i + 1));
+}
