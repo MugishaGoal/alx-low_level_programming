35b830609b4ed19252b2ca3b6eccf205aaeffd05
MugishaGoal <degaullemugisha@gmail.com>
2023-03-12 06:44:03 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,17 @@ int main(int argc, char **argv)
 {
 	int num1, num2, result;
 
+	if (argc == 3)
+	{
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
+	}
 	return (0);
-	if (argc == 3)
+	else
 	{
 	printf("Error\n");
-	}
 	return (1);
+	}
 }
