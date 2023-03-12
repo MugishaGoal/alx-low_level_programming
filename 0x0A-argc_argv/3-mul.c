8760cd688c84d8a002f36ebeaa0a06619170ea3e
MugishaGoal <degaullemugisha@gmail.com>
2023-03-12 05:56:49 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: counts the number of arguments
+ * @argv: an array of the string
+ * Return: 0 Always (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+	if (argc != 3)
+	{
+	printf("Error\n");
+	}
+	return (1);
+}
