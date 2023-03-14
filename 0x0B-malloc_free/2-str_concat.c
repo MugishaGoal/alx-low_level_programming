6ad40ac01fcb90df711c84f6f8c8951e16622f65
MugishaGoal <degaullemugisha@gmail.com>
2023-03-14 12:12:40 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,25 +21,29 @@ char *str_concat(char *s1, char *s2)
 	s2 = "";
 
 	while (s1[i])
+	{
 	i++;
-
+	}
 	while (s2[j])
+	{
 	j++;
-
+	}
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 	if (s == NULL)
+	{
 	return (NULL);
-
+	}
+	k = 0;
 	while (k < l)
 	{
-	if (k <= i)
-	s[k] = s1[k];
-
-	if (k >= i)
+	if (k < l)
 	{
-	s[k] = s2[j];
-	j++;
+	s[k] = s1[k];
+	}
+	else
+	{
+	s[k] = s2[k - i];
 	}
 	k++;
 	}
