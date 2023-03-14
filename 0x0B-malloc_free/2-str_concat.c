cb01db141a12d80699f82963bd1edf6a2500e389
MugishaGoal <degaullemugisha@gmail.com>
2023-03-14 11:11:06 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * str_concat - concatentes two strings
@@ -16,34 +15,33 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-	{
 	s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 	s2 = "";
-	}
+
 	while (s1[i])
-	{
 	i++;
-	}
+
 	while (s2[j])
-	{
 	j++;
-	}
+
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 	if (s == NULL)
-	{
 	return (NULL);
-	}
+
 	while (k < l)
 	{
 	if (k <= i)
 	s[k] = s1[k];
+
 	if (k >= i)
+	{
 	s[k] = s2[j];
 	j++;
+	}
+	k++;
 	}
 	s[k] = '\0';
 	return (s);
