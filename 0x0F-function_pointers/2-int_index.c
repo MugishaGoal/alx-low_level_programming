53794e6601700415c25d7a8ecede4ba0c6b2ee7a
MugishaGoal <degaullemugisha@gmail.com>
2023-03-23 03:52:56 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,11 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * int_index - ...
- * int_index: returns the index of the first element
+ * int_index -  searches for an integer
  * @size: ...
  * @array: ...
  * @cmp: ...
+ *
+ * Return: index of the first element for which cmp function
+ * does not return 0, -1 if no element matches or size <= 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
