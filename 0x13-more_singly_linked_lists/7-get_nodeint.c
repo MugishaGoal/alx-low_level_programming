509f7b55cbf9a7052532e494a3875e49709dc77a
MugishaGoal <degaullemugisha@gmail.com>
2023-04-05 02:43:52 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,8 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *ptr = head;
 
 	for (i = 0; i < index; i++)
-	if (ptr == NULL)
-	return (NULL);
+	if (ptr != NULL)
 	ptr = ptr->next;
 	return (ptr);
 }
