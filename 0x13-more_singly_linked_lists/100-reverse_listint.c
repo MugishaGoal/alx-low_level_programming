ed34c199746355d2c0a2ccafb0dd8261137ffe02
MugishaGoal <degaullemugisha@gmail.com>
2023-04-05 15:05:09 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,7 +5,7 @@
 /**
  * reverse_listint - reverses a listint_t linked list
  * @head: a double pointer to the first node
- * Rteurn: a pointer to the first node of the reversed list
+ * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
