362aae71c575dc7f01e4f8e7ca8163009ddf560e
Mugisha Goal <degaullemugisha@gmail.com>
2023-05-08 06:28:39 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes a character
+ * @c: a character to be written
+ *
+ * Return: 1 on success, -1 on error and the error is set
+ * appropriately
+ */
+int _putchar(char c)
+{
+	return (write (1, &c 1));
+}
